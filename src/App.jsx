7f362e0dcefc8415a36f0bed6fb01d1503d565f2
haseeb0307 <haseebfalkaram@gmail.com>
2025-04-07 14:54:57 +0500

    remove x

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
       <div className='main-div'>
         <div className='form'>
           <h2>Weather App</h2>
-          <input type="text" value={city} onChange={handleChange} placeholder='Enter City name 'x/>
+          <input type="text" value={city} onChange={handleChange} placeholder='Enter City name '/>
 
           
           <br></br>
